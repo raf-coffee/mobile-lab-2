f0436268b25abf51b02d7070257a3d3f45b7ac2e
raf-coffee <pavel.hexlet@mail.ru>
2024-11-13 22:13:46 +0300

    add pull to refresh

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,35 @@
-import { ScrollView, StyleSheet, FlatList } from "react-native";
+import { ScrollView, RefreshControl, StyleSheet, FlatList } from "react-native";
 import LanguageList from "@/components/LanguageList";
 import data from "@/data";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    console.log("Refresh");
+
+    setTimeout(() => {
+      setRefreshing(false);
+      console.log("Refresh end");
+    }, 500);
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <LanguageList
-            language={item.language}
-            experience={item.experience}
-            logo={item.logo}
-          />
-        )}
-      />
-    </ScrollView>
+    <FlatList
+      style={styles.container}
+      data={data}
+      renderItem={({ item }) => (
+        <LanguageList
+          language={item.language}
+          experience={item.experience}
+          logo={item.logo}
+        />
+      )}
+      refreshing={refreshing}
+      onRefresh={handleRefresh}
+    />
   );
 }
 
